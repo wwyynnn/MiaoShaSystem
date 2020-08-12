90bde379b98ff957b3edc1c7d747b230580e49cf
Yannan_Wang <2435173W@student.gla.ac.uk>
2020-08-12 17:09:20 +0800

    update test folder
    上传test文件夹

---
+++ src/test/java/com/miaoshaSystem/AppTest.java
@@ -0,0 +1,20 @@
+package com.miaoshaSystem;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
